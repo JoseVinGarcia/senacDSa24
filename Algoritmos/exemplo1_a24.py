5c55c485eb7da18865fc1bb0be0bae54e90008e5
JoseVinGarcia <josevingarcia2020@gmail.com>
2024-12-03 20:40:14 -0300

    Commit inicial

--- Algoritmos/exemplo1_a24.py
+++ Algoritmos/exemplo1_a24.py
@@ -1,16 +1,45 @@
 # AULA 24 - Big data
 
 import pandas as pd
+import polars as pl
 from datetime import datetime
 
+# TRY COM PANDAS
+# try:
+#     ENDERECO_DADOS = r'./dados/'
+
+#     # hora de inicio
+#     hora_import = datetime.now()
+
+#     df_janeiro = pd.read_csv(ENDERECO_DADOS + '202401_NovoBolsaFamilia.csv',sep=';',encoding='iso-8859-1')
+
+#     print(df_janeiro.head())
+
+#     # hora final
+#     hora_impressao = datetime.now()
+
+#     print(f"Tempo de execução: {hora_impressao - hora_import}")
+
+
+# except ImportError as e:
+#     print(f"Erro ao obter dados: {e}")
+
+# TRY COM POLARS
 try:
     ENDERECO_DADOS = r'./dados/'
 
     # hora de inicio
     hora_import = datetime.now()
 
-    df_janeiro = pd.read_csv(ENDERECO_DADOS + '202401_NovoBolsaFamilia.csv',sep=';',encoding='iso-885')
+    df_janeiro = pl.read_csv(ENDERECO_DADOS + '202401_NovoBolsaFamilia.csv',separator=';',encoding='iso-8859-1')
 
     print(df_janeiro.head())
+
+    # hora final
+    hora_impressao = datetime.now()
+
+    print(f"Tempo de execução: {hora_impressao - hora_import}")
+
 except ImportError as e:
     print(f"Erro ao obter dados: {e}")
+
